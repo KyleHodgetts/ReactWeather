2614031a5d8ec43c7d7b859c745438697b81daf2
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-06-01 22:06:24 +0100

    Styled examples page

--- app/components/pages/Examples.jsx
+++ app/components/pages/Examples.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default () => {
-  return (<h3>Examples</h3>); 
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>A few example locations for you to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=london'>London</Link>
+        </li>
+        <li>
+          <Link to="/?location=warsaw">Warsaw, PL</Link>
+        </li>
+      </ol>
+    </div>
+  );
 }
