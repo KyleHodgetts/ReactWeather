a8db7a7935afcc3972725ce6285c9cba08cb1d12
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-06-01 20:30:13 +0100

    Prep repo for heroku

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 // Create App
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  }
+  else {
+    res.redirect(`http://${req.hostname}${req.url}`);
+  }
+});
 
 app.use(express.static('public'));
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server is running on 3000");
+
+app.listen(PORT, function () {
+  console.log(`Server is running on ${PORT}`);
 });
